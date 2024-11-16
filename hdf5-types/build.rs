ef23e6d1b9f09aa145b0b0e97dcbdeb554f97911
James McNally <james@wiresmithtech.com>
2024-11-16 08:27:46 +0000

    fix: Add cargo feature checks to hdf5-types
    Needed to add the check cfg for the version features in the hdf-types
    library

--- hdf5-types/build.rs
+++ hdf5-types/build.rs
@@ -4,6 +4,11 @@ fn main() {
     if std::env::var_os("DEP_HDF5_MSVC_DLL_INDIRECTION").is_some() {
         println!("cargo::rustc-cfg=windows_dll");
     }
+
+    // Declare the known HDF5 versions we might feature flag on
+    // in this crate.
+    println!("cargo::rustc-check-cfg=cfg(feature, values(\"1.12.0\"))");
+
     for (key, _) in std::env::vars() {
         if key.starts_with("DEP_HDF5_VERSION_") {
             let version = key.trim_start_matches("DEP_HDF5_VERSION_").replace("_", ".");
