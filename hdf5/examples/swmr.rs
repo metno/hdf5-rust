a8c3907d94c93deb369e7700982cc417b51626fd
Magnus Ulimoen <magnusu@met.no>
2025-04-29 11:47:49 +0200

    Add backwards compatibility

--- hdf5/examples/swmr.rs
+++ hdf5/examples/swmr.rs
@@ -1,5 +1,7 @@
+#[cfg(feature = "1.10.2")]
 use hdf5_metno as hdf5;
 
+#[cfg(feature = "1.10.2")]
 fn reader() {
     let file = hdf5::File::open_as("swmr.h5", hdf5::OpenMode::ReadSWMR).unwrap();
     println!("Reader: Opened file");
@@ -17,23 +19,31 @@ fn reader() {
 }
 
 fn main() {
-    let file =
-        hdf5::File::with_options().with_fapl(|fapl| fapl.libver_v110()).create("swmr.h5").unwrap();
+    #[cfg(not(feature = "1.10.2"))]
+    println!("This examples requires hdf5 >= 1.10.2 to enable SWMR and set libver_bounds");
 
-    let var = file.new_dataset::<u8>().shape((0.., 5)).create("foo").unwrap();
+    #[cfg(feature = "1.10.2")]
+    {
+        let file = hdf5::File::with_options()
+            .with_fapl(|fapl| fapl.libver_v110())
+            .create("swmr.h5")
+            .unwrap();
 
-    file.start_swmr().unwrap();
-    println!("Writer: Wrote file");
+        let var = file.new_dataset::<u8>().shape((0.., 5)).create("foo").unwrap();
 
-    let thread = std::thread::spawn(|| reader());
+        file.start_swmr().unwrap();
+        println!("Writer: Wrote file");
 
-    for i in 0..5 {
-        var.resize((i + 1, 5)).unwrap();
-        var.write_slice(&[i, i, i, i, i], (i, 0..)).unwrap();
-        var.flush().unwrap();
-        println!("Writer: Wrote {i}");
-        std::thread::sleep(std::time::Duration::from_secs(5));
+        let thread = std::thread::spawn(|| reader());
+
+        for i in 0..5 {
+            var.resize((i + 1, 5)).unwrap();
+            var.write_slice(&[i, i, i, i, i], (i, 0..)).unwrap();
+            var.flush().unwrap();
+            println!("Writer: Wrote {i}");
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+
+        thread.join().unwrap();
     }
-
-    thread.join().unwrap();
 }
